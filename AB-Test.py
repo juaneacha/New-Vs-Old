2e3cd5c67cf848f6661c679131d02fc9fef92153
juaneacha <70673871+juaneacha@users.noreply.github.com>
2025-02-28 16:22:51 -0500

    Update AB-Test.py

--- AB-Test.py
+++ AB-Test.py
@@ -17,14 +17,17 @@ newMenu = df['Gross Earnings'][7:13].astype(float)
 #print(newMenu)
 
 # perform mann whitney test 
-stat, p_value = mannwhitneyu(oldMenu, newMenu) 
+stat, p_value = mannwhitneyu(newMenu, oldMenu, alternative='greater') 
 print('Statistics=%.2f, p=%.2f' % (stat, p_value)) 
 
 # Level of significance 
 alpha = 0.05
 
 # conclusion 
+#The null hypothesis is that revenue increases by 5 - 10% when the menu is changed
+#The alternate hypothesis is that revenue does not increase by at least 5%
+
 if p_value < alpha: 
-    print('Reject Null Hypothesis (Significant difference between two samples)') 
+    print('Reject Null Hypothesis (There IS NOT an a increase in revenue)') 
 else: 
-    print('Do not Reject Null Hypothesis (No significant difference between two samples)')
+    print('Do not Reject Null Hypothesis (There IS an a increase in revenue)')
